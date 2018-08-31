017418e6a3c4f8a72d0975fbba74b48f79e14a69
Azkalf <azkalf@gitlab.com>
2018-08-31 14:56:42 +0700

    initial commit

--- laravel/resources/views/layouts/adminbsb.blade.php
+++ laravel/resources/views/layouts/adminbsb.blade.php
@@ -172,10 +172,10 @@
             <!-- Footer -->
             <div class="legal">
                 <div class="copyright">
-                    &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+                    {!! $setting->copyright !!}
                 </div>
                 <div class="version">
-                    <b>Version: </b> 1.0.5
+                    {!! $setting->version !!}
                 </div>
             </div>
             <!-- #Footer -->
