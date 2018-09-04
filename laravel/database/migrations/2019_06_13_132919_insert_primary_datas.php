9e4a7bc2dfd42059ce69ef100d71231d967edf25
Azkalf <azkalf@gitlab.com>
2018-09-04 11:49:48 +0700

    update Readme

--- laravel/database/migrations/2019_06_13_132919_insert_primary_datas.php
+++ laravel/database/migrations/2019_06_13_132919_insert_primary_datas.php
@@ -59,9 +59,9 @@ class InsertPrimaryDatas extends Migration
         ]);
 
         DB::table('menus')->insert([
-            ['id'=>1, 'title'=>'CONFIGURATION', 'icon'=>'settings', 'parent'=>0, 'url'=>'#', 'order'=>0],
+            ['id'=>1, 'title'=>'KONFIGURASI', 'icon'=>'settings', 'parent'=>0, 'url'=>'#', 'order'=>0],
             ['id'=>2, 'title'=>'Menu', 'icon'=>'menu', 'parent'=>1, 'url'=>'menu', 'order'=>1],
-            ['id'=>3, 'title'=>'Group', 'icon'=>'group', 'parent'=>1, 'url'=>'group', 'order'=>2],
+            ['id'=>3, 'title'=>'Grup', 'icon'=>'group', 'parent'=>1, 'url'=>'group', 'order'=>2],
             ['id'=>4, 'title'=>'User', 'icon'=>'person', 'parent'=>1, 'url'=>'user', 'order'=>3],
             ['id'=>5, 'title'=>'Setting', 'icon'=>'settings', 'parent'=>1, 'url'=>'setting', 'order'=>4],
         ]);
